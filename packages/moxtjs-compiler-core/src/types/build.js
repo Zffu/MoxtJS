eb05ccc678230588983fbd8d70f8ce4b887796c9
Zffu <radi0o0@outlook.com>
2023-06-07 17:24:44 +0200

    Fixed typo Time instead of Date in build object deploy-compiler-core

--- packages/moxtjs-compiler-core/src/types/build.js
+++ packages/moxtjs-compiler-core/src/types/build.js
@@ -3,7 +3,7 @@ const crypto = require("crypto")
 class MoxtJSBuild {
     constructor(path) {
         this.path = path;
-        this.start_timestamp = new Time().getTime();
+        this.start_timestamp = new Date().getTime();
         this.build_uuid = crypto.randomUUID();
         this.state = "CHEKING_CAN_COMPILE"
         this.build_sucess = false;
@@ -11,7 +11,7 @@ class MoxtJSBuild {
     }
     end(sucess) {
         this.build_sucess = sucess;
-        this.stop_timestamp = new Time().getTime()
+        this.stop_timestamp = new Date().getTime()
         if(sucess) {
             this.state = "STOPPED_SUCESS"
         }
